7cf05988e02f705202f738a6d5d4b3a0ebcdc57d
Rob Donoghue <rdonoghue@gmail.com>
2023-03-15 18:15:56 -0400

    Fully working, ready to watch videos

--- dbblog/routes/blog.js
+++ dbblog/routes/blog.js
@@ -10,31 +10,37 @@ router.get("/", function (req, res) {
   res.redirect("posts");
 });
 
+// List of Posts
+
 router.get("/posts", async function (req, res) {
   const [postList] = await db.query(`
   SELECT posts.*, posts.id as id, authors.name as authorname 
   FROM posts 
   JOIN authors on posts.author_id = authors.id `);
   await res.render("posts-list", { postList: postList });
-
-  console.log([postList]);
 });
+
+// New Post Form
 
 router.get("/new-post", async function (req, res) {
   const [authors] = await db.query("SELECT * FROM authors");
   res.render("create-post", { authors: authors });
 });
 
+// Edit Post Form
+
 router.get("/edit-post/:id", async function (req, res) {
   const postId = req.params.id;
   const [postDetails] = await db.query(
-    `SELECT * 
+    `SELECT posts.*, authors.name as name 
     FROM POSTS JOIN authors ON posts.author_id = authors.id 
     WHERE posts.id= ?`,
     [postId]
   );
   res.render("edit-post", { postDetails: postDetails[0] });
 });
+
+// Write New Post Entry to DB
 
 router.post("/posts", async function (req, res) {
   const data = [
@@ -76,10 +82,18 @@ router.post("/edit", async function (req, res) {
     req.body.author_id,
     req.body.id,
   ];
-  await db.query(
-    "UPDATE posts SET title=?, summary=?, body=?, author_id=? WHERE id=?",
-    [data[0], data[1], data[2], data[3], data[4]]
-  );
+  console.log(data);
+
+  await db
+    .query(
+      "UPDATE posts SET title=?, summary=?, body=?, author_id=? WHERE id=?",
+      [data[0], data[1], data[2], data[3], data[4]],
+      function () {
+        console.log("It ran");
+      }
+    )
+    .catch(handleError);
+
   res.redirect("/posts");
 });
 
@@ -90,17 +104,18 @@ router.post("/delete", async function (req, res) {
   res.redirect("/posts");
 });
 
-// router.get("/details/:id", function (req, res) {
-//   const postid = req.params.id;
-
 router.get("/details/:id", async function (req, res) {
   const postId = req.params.id;
   const [postDetails] = await db.query(
     "SELECT posts.*, authors.name as name FROM POSTS JOIN  authors ON posts.author_id = authors.id  WHERE posts.id= ?",
     [postId]
   );
-
   res.render("post-detail", { postDetails: postDetails[0] });
 });
 
 module.exports = router;
+
+function handleError(err) {
+  console.log("Crud");
+  console.log(err);
+}
--- scratchspace/script.js
+++ scratchspace/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const sampleArray = [1, 2, 3];
+console.log(sampleArray);
+
+console.log(sampleArray.reduce(myFunc, 10));
+function myFunc(total, num) {
+  console.log(total);
+  return total + num;
+}
