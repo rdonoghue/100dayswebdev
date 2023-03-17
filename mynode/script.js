e85011309f27567bbeaa6b6781206c3e69745cdc
Rob Donoghue <rdonoghue@gmail.com>
2023-03-16 21:19:56 -0400

    nothing of note

--- mynode/script.js
+++ mynode/script.js
@@ -1,18 +1,20 @@
 "use strict";
 const fs = require("fs");
 const cards = require("./trumpdata.js");
-
-const cardList = cards.amberCards;
-
-console.log(Object.keys(cardList).length);
+const cardList = cards.allCards;
+// console.log(cardList);
 
 const cardJSON = JSON.stringify(cardList);
+// console.log(cardJSON);
 
 // const dataFilePath = path.join(__dirname, "data", "cards.json");
 const dataFilePath = ".";
 
 // const fileData = fs.readFileSync(dataFilePath);
+fs.writeFile("cards.json", cardJSON, function (err, file) {
+  if (err) throw err;
+  console.log("Saved!");
+});
+// fs.appendFile("cards.json", cardJSON);
 
-// fs.open("cards.json");
-
-fs.writeFileSync("cards.json", cardJSON);
+// fs.writeFileSync("cards.json", cardJSON);
